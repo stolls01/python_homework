e56e4f2d99bb67711a5f85af44ea67ce203d5a5b
Sophia Stoll <a12030007@unet.univie.ac.at>
2024-11-13 12:56:57 +0100

    Add ini3.py

---
+++ rosalind_exercises/ini3.py
@@ -0,0 +1,11 @@
+# Given: A string s of length at most 200 letters and four integers a, b, c and d.
+# Return: The slice of this string from indices a
+# through b and c through d (with space in between), inclusively.
+# In other words, we should include elements s[b] and s[d] in our slice.
+
+s = ""
+a =
+b =
+c =
+d =
+print(s[a:b+1], s[c:d+1])
