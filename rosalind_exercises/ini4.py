a6da4bf78c24c3c14014c91777a67a519d181da7
Sophia Stoll <a12030007@unet.univie.ac.at>
2024-11-13 11:27:17 +0100

    Make folder for rosalind

---
+++ rosalind_exercises/ini4.py
@@ -0,0 +1,15 @@
+a =
+b =
+
+integer_list = list(range(a, b+1))
+
+if (a % 2) == 0:
+    odd_integers = integer_list[1::2]
+else:
+    odd_integers = integer_list[0::2]
+
+odd_integer_sum = 0
+for integer in odd_integers:
+    odd_integer_sum = odd_integer_sum + integer
+
+print(odd_integer_sum)
