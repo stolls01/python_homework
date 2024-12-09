989c89dfae78b2bad41ae20f1b419fdede601262
Sophia Stoll <a12030007@unet.univie.ac.at>
2024-12-09 18:27:10 +0100

    Day 9

--- advent_of_code/9.py
+++ advent_of_code/9.py
@@ -33,111 +33,20 @@ block_flipped = orig_block[::-1]
 #print("".join(block_flipped))
 # 998888.777.6666.5555.44.333...2...111...00
 
+updated_block = orig_block
+for f in block_flipped:
+    if f != ".":
+        for num, o in enumerate(updated_block, start = 0):
+            if o == ".":
+                print("num", num)
+                updated_block[num] = f
+                break
+    if f == ".":
+        updated_block = updated_block[:-1]
 
-# get the indeces of file blocks
-numberblocks = []
-for num, x in enumerate(block_flipped, start = 0):
-    status = False
-    if x == ".":
-        continue
-    if x != ".":
-        for y in range(10):
-            if num+y < len(block_flipped)-1 and block_flipped[num + y] != x and status is False:
-                filestart = num
-                fileend = num + y
-                if fileend not in [b[1] for b in numberblocks]:
-                    numberblocks.append([filestart, fileend])
-                status = True
+result = 0
+for num, u in enumerate(updated_block, start = 0):
+    result += num * int(u)
 
-print(numberblocks)
-
-#################
-# walk along original block -- if encountering a . : calculate distance  to next non-dot
-# go through all numberblocks: if they cover less distance than dot-distance: replace dots with that numberblock
-block_two = copy.deepcopy(orig_block)
-
-status = False
-for i, b in enumerate(numberblocks):
-    block_length = b[1]-b[0]
-    for num, x in enumerate(block_two, start = 0):
-        if x == ".":
-            for y in range(9):
-                if num+y < len(block_two)-1 and block_two[num + y] != '.' and status is False:
-                    status = True
-                    dotstart = num
-                    dotend = num + y
-                    dot_length = y
-                    status = True
-            if b[1]-b[0] <= dot_length:
-                block_two[dotstart:(dotstart+b[1]-b[0])] = block_flipped[b[0]:b[1]]
-
-
-            # # print(block_two[dotstart:(dotstart+b[1]-b[0])])
-            # if b[1]-b[0] > dot_length:
-            #     continue
-            # if b[1]-b[0] <= dot_length:
-            #     block_two[dotstart:(dotstart+b[1]-b[0])] = block_flipped[b[0]:b[1]]
-            #     # remove numberblock from blocks list
-            #     for j in b:
-            #         for k in range(dotend, len(block_two)):
-            #             if block_two[k] == block_flipped[j]:
-            #                 block_two[k] = "."
-            #     status = True
-            #     break
-print("numberblocks", numberblocks)
-
-print("UPDATED", block_two)
-print("".join(block_two))
-# print("".join(block_two))
-# print("".join(block_flipped))
-# for f in block_flipped:
-
-
-#     if f != ".":
-#         for num, o in enumerate(updated_block, start = 0):
-#             if o == ".":
-#                 print("num", num)
-#                 updated_block[num] = f
-#                 break
-#     if f == ".":
-#         updated_block = updated_block[:-1]
-
-# result = 0
-# for num, u in enumerate(updated_block, start = 0):
-#     result += num * int(u)
-
-# print(result)
-
-
-
-
-
-
-# for d in dots:
-#     print(orig_block[d[0]:d[1]])
-
-# for d in blocks:
-#     print(block_flipped[d[0]:d[1]])
-
-
-
-
-
-
-# PART ONE
-# updated_block = orig_block
-# for f in block_flipped:
-#     if f != ".":
-#         for num, o in enumerate(updated_block, start = 0):
-#             if o == ".":
-#                 print("num", num)
-#                 updated_block[num] = f
-#                 break
-#     if f == ".":
-#         updated_block = updated_block[:-1]
-
-# result = 0
-# for num, u in enumerate(updated_block, start = 0):
-#     result += num * int(u)
-
-# print(result)
+print("".join(updated_block))
+print(result)
